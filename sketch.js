a5bc7714b7f4459983f6593a5e393c0c1bec78a5
Malachi <maflack20@wlapcs.org>
2018-02-08 04:35:37 +0000

    done

--- sketch.js
+++ sketch.js
@@ -1,8 +1,24 @@
+var song;
+var button;
+
+function preload(){
+    song=loadSound("sound/laurynhill-doowop.mp3")
+}
+
 function setup(){
     createCanvas(600,400);
     background(0);
+    
+    button=createButton("Play")
+    button.mousePressed(togglePlaying);
+    button.position(20, height-10),
+    song.play();
 }
 
 function draw(){
+    
+}
+
+function togglePlaying(){
     
 }
